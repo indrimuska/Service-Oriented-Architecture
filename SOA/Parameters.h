a998a1ad41392acc71b2f0a600222b645571afa1
Indri Muska <indrimuska@gmail.com>
2012-04-01 17:30:49 +0200

    Eliminato file SOA/types.h

---
+++ SOA/Parameters.h
@@ -0,0 +1,14 @@
+//
+//  Parameters.h
+//  Service Oriented Architecture
+//
+//  Created by Indri Muska on 01/04/12.
+//  Copyright (c) 2012 Indri Muska. All rights reserved.
+//
+
+#ifndef Service_Oriented_Architecture_Parameters_h
+#define Service_Oriented_Architecture_Parameters_h
+
+
+
+#endif
