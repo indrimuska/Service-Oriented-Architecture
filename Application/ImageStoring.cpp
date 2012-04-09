71643d1edc0425803e1348eaeff756a385a82898
Indri Muska <indrimuska@gmail.com>
2012-04-09 15:14:52 +0200

    Prima versione del client e dei server

---
+++ Application/ImageStoring.cpp
@@ -0,0 +1,9 @@
+//
+//  ImageStoring.cpp
+//  Service Oriented Architecture
+//
+//  Created by Indri Muska on 09/04/12.
+//  Copyright (c) 2012 Indri Muska. All rights reserved.
+//
+
+#include <iostream>
---
+++ Application/ImageStoring.h
@@ -0,0 +1,14 @@
+//
+//  ImageStoring.h
+//  Service Oriented Architecture
+//
+//  Created by Indri Muska on 09/04/12.
+//  Copyright (c) 2012 Indri Muska. All rights reserved.
+//
+
+#ifndef Service_Oriented_Architecture_ImageStoring_h
+#define Service_Oriented_Architecture_ImageStoring_h
+
+
+
+#endif
